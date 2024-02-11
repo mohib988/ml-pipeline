6b5d99a9b1417d5df4ed196b86d17312037cb7da
mohib <qureshimohib819@gmail.com>
2024-02-11 15:35:11 +0500

    added evaluation

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import logging
 from sklearn.metrics import (
     roc_curve,
     roc_auc_score,
@@ -11,40 +14,44 @@ from sklearn.metrics import (
 from zenml import step
 
 class ClassificationEvaluator:
-    def __init__(self, model, X_test, y_test):
+    def __init__(self, model:RandomForestClassifier, X_test:pd.DataFrame, y_test:pd.Series):
         self.model = model
         self.X_test = X_test
         self.y_test = y_test
         self.y_predicted = self.model.predict(self.X_test)
-    def plot_pr_curve(self):
+    def pr_matrix(self):
         average_precision = average_precision_score(self.y_test, self.y_predicted)
         precision, recall, _ = precision_recall_curve(self.y_test, self.y_predicted)
-        print("precision:",precision)
-        print("recall:",recall)
-        print("average_precision:",average_precision)
+        logging.info("precision:",precision)
+        logging.info("recall:",recall)
+        logging.info("average_precision:",average_precision)
         probs = self.model.predict_proba(self.X_test)
         false_positive_rate, true_positive_rate, _ = roc_curve(self.y_test, probs[:, 1])
         roc_auc = roc_auc_score(self.y_test, probs[:, 1])
-        print("false_positive_rate:",false_positive_rate)
-        print("true_positive_rate:",true_positive_rate)
-        print("probs:",probs)
-        print("roc_auc:",roc_auc)
+        logging.info("false_positive_rate:",false_positive_rate)
+        logging.info("true_positive_rate:",true_positive_rate)
+        logging.info("probs:",probs)
+        logging.info("roc_auc:",roc_auc)
     def evaluate_classification(self):
         # Calculate metrics
         accuracy = accuracy_score(self.y_test, self.y_predicted)
         probs = self.model.predict_proba(self.X_test)
         auc_roc_score = roc_auc_score(self.y_test, probs[:, 1])
 
-        # Print metrics
-        print("Accuracy Score:", accuracy)
-        print("AUC ROC score:", auc_roc_score)
-        print('Classification report:\n', classification_report(self.y_test, self.y_predicted))
-        print('Confusion matrix:\n', confusion_matrix(y_true=self.y_test, y_pred=self.y_predicted))
+        # logging.info metrics
+        logging.info("Accuracy Score:", accuracy)
+        logging.info("AUC ROC score:", auc_roc_score)
+        logging.info('Classification report:\n', classification_report(self.y_test, self.y_predicted))
+        logging.info('Confusion matrix:\n', confusion_matrix(y_true=self.y_test, y_pred=self.y_predicted))
 
 
 @step
-def evaluate(model, X_test, y_test):
-    classifier = ClassificationEvaluator(model, X_test, y_test)
-    classifier.plot_pr_curve()
-    classifier.plot_roc_curve()
-    classifier.evaluate_classification()
+def evaluate(model:RandomForestClassifier, X_test:pd.DataFrame, y_test:pd.Series):
+    try:
+        classifier = ClassificationEvaluator(model, X_test, y_test)
+        classifier.pr_matrix()
+        classifier.evaluate_classification()
+    except Exception as e:
+        logging.error("Error in evaluating the model", e)
+        raise e
+    
--- steps/model_train.py
+++ steps/model_train.py
@@ -7,6 +7,13 @@ from sklearn.ensemble import RandomForestClassifier
 
 @step
 def train_model(X_train: pd.DataFrame,y_train:pd.Series) -> RandomForestClassifier:
+    '''Train the model
+    Args:
+        X_train: pd.DataFrame: Training data
+        y_train: pd.Series: Training labels
+    Returns:
+        RandomForestClassifier: Trained model
+    '''
     model=RandomForestModel()
     model =model.train_random_forest(X_train, y_train)
     logging.info("Training the model")
