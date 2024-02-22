1d36aacf4b0523ccd0c43080abd54887e54b3b0e
mohib <qureshimohib819@gmail.com>
2024-02-22 09:27:51 +0500

    added requiremt.txt

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,4 +1,5 @@
 from zenml import pipeline
+
 from steps.ingest_data import ingest_data
 from steps.clean_data import clean_data,split_data
 from steps.model_train import train_model
@@ -6,8 +7,8 @@ from steps.evaluation import evaluate
 from src.models.isolation_forest import run_isolation_forest    
 from src.merge.merge_anomaly import merge_anomaly
 from src.models.loc import run_loc
-from src.models.fbprophet import run_prophet
-@pipeline(enable_cache=True)
+# from src.models.fbprophet import run_prophet
+@pipeline(enable_cache=False)
 def training_pipeline(path:str):
     # Define pipeline here
     df=ingest_data(path)
@@ -16,10 +17,10 @@ def training_pipeline(path:str):
     
     df1=run_isolation_forest(df)
     df2=run_loc(df)
-    df3=run_prophet(df)
-    df_merge=merge_anomaly(df1,df2,df3)
+    # df3=run_prophet(df)
+    df_merge=merge_anomaly(df1,df2,df1)
     X_train, X_test, y_train, y_test=split_data(df_merge)
-    # localOutlierDetection.fit_predict(df)
 
     model=train_model(X_train, y_train)
     a=evaluate(model, X_test, y_test)
+    return df_merge
--- run_pipeline.py
+++ run_pipeline.py
@@ -2,9 +2,11 @@ from pipelines.training_pipeline import training_pipeline
 import mlflow
 from zenml.client import Client
 
+def main():
+    print(Client().active_stack.experiment_tracker.get_tracking_uri())
+    return training_pipeline(path='data/AnomalyDetection.csv')
 
 if __name__ == '__main__':
+    main()
     # Run the training pipeline
-    print(Client().active_stack.experiment_tracker.get_tracking_uri())
-    training_pipeline(path='data/AnomalyDetection.csv')
  
