4c02b0662a65a8a0ad7a6718367f8aad6fa957fe
mohib <qureshimohib819@gmail.com>
2024-02-25 12:10:12 +0500

    add input

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,5 +1,6 @@
 from zenml import pipeline
-
+import pandas as pd
+import numpy as np
 from steps.ingest_data import ingest_data
 from steps.clean_data import clean_data,split_data
 from steps.model_train import train_model
@@ -10,10 +11,10 @@ from src.models.loc import run_loc
 from src.models.hdbscan import run_hdb_clustering
 # from src.models.fbprophet import run_prophet
 @pipeline(enable_cache=False)
-def training_pipeline(path:str):
+def training_pipeline(df1:pd.DataFrame):
     # Define pipeline here
-    df=ingest_data(path)
-    df=clean_data(df)
+    # df=ingest_data(path)
+    df=clean_data(df1)
     
     anomaly1=run_isolation_forest(df)
     anomaly2=run_hdb_clustering(df)
--- run_pipeline.py
+++ run_pipeline.py
@@ -1,10 +1,11 @@
 from pipelines.training_pipeline import training_pipeline
 import mlflow
+import pandas as pd
 from zenml.client import Client
 
-def main():
+def main(df:pd.DataFrame):
     print(Client().active_stack.experiment_tracker.get_tracking_uri())
-    return training_pipeline(path='data/AnomalyDetection.csv')
+    return training_pipeline(pd)
 
 if __name__ == '__main__':
     main()
