acf9a001302530f5e608f5f0d25af2866c4485d0
mohib <qureshimohib819@gmail.com>
2024-02-26 14:27:23 +0500

    remove pandas core error

--- run_pipeline.py
+++ run_pipeline.py
@@ -6,8 +6,10 @@ import pandas as pd
 from zenml.client import Client
 
 def main(df):
+    df_dict_list = df.to_dict(orient='records')
+    df_json_str = json.dumps(df_dict_list)
     print(Client().active_stack.experiment_tracker.get_tracking_uri())
-    return training_pipeline(df)
+    return training_pipeline(df_json_str)
 
 if __name__ == '__main__':
     df=pd.read_csv("./data/AnomalyDetection.csv")
